371dbe927cfa04221142ba92fbfc9e23996d0d2d
Ryuuga <154620046+LordRyuuga@users.noreply.github.com>
2023-12-25 13:28:18 -0500

    Update ampharos.asm

--- data/pokemon/base_stats/ampharos.asm
+++ data/pokemon/base_stats/ampharos.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  90,  75,  85,  55, 115,  90 ; 510 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db  95,  75,  90,  65, 125,  95 ; 545 BST
+	db  95,  75,  90,  65, 135,  95 ; 555 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
@@ -18,7 +18,7 @@ endc
 	db NO_ITEM, NO_ITEM ; held items
 	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
-	abilities_for AMPHAROS, STATIC, STATIC, MOLD_BREAKER
+	abilities_for AMPHAROS, STATIC, VOLT_ABSORB, MOLD_BREAKER
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_GROUND ; egg groups
 
